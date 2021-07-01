b38a578f3062942c0b8385fb8fed7cb4cb72c045
jauhhari <ervanadiwijaya@gmail.com>
2021-07-01 08:27:24 +0700

    OK

---
+++ tugas11.js
@@ -0,0 +1,16 @@
+function operator() {
+
+    var a = 2;
+    var b = 4;
+    var c = 5;
+    var d = 10;
+
+    //with these number we should get '25' numb
+
+    value = b * c + d / a;
+    
+    console.log(value);
+    
+}
+
+operator();
